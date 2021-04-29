a5ad86818dfd2dac9cc1ad2af03f73cf721199de
mabatule1999 <maba1998a@gmail.com>
2021-04-29 15:11:04 -0400

    fef

---
+++ GearBox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_4_Facade_
+{
+    class GearBox
+    {
+    }
+}
---
+++ RacquetShop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_4_Facade_
+{
+    public interface RacquetShop
+    {
+        void ModelNo();
+        void Price();
+    }
+}
